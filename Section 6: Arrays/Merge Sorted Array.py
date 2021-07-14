a5c2e120d70177c424800115b2d40126dd407ed4
pedrazac95 <86867284+pedrazac95@users.noreply.github.com>
2021-07-13 22:29:07 -0500

    Create Merge Sorted Array.py

---
+++ Section 6: Arrays/Merge Sorted Array.py
@@ -0,0 +1,12 @@
+def mergeSortedArrays( array1:list , array2:list) -> list:
+    new_array = array1 + array2
+    new_array.sort()
+    
+    print (new_array)
+
+    
+
+
+
+
+mergeSortedArrays([0,3,4,31], [3,4,6,30]);
